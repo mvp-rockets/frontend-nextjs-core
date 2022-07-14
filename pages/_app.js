ea9a7321c567981d62be54440795a65af3c3e6bf
Priyansh <priyansh.jain@napses.com>
2022-07-14 18:07:36 +0530

    Flagsmith Feature Added

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,4 @@
 import '../styles/globals.css';
-import { appWithTranslation } from 'next-i18next';
-import nextI18NextConfig from '../next-i18next.config.js';
 import { FlagsmithProvider } from 'flagsmith/react';
 import flagsmith from 'flagsmith/isomorphic';
 
@@ -14,9 +12,9 @@ function MyApp({ Component, pageProps, flagsmithState }) {
 
 MyApp.getInitialProps = async () => {
   await flagsmith.init({
-   environmentID:'<YOUR_ENVIRONMENT_ID>',
+   environmentID:process.env.FLAGSMITH_KEY,
   });
   return { flagsmithState: flagsmith.getState() };
  };
 
-export default appWithTranslation(MyApp, nextI18NextConfig)
+export default MyApp;
