92981c3766adf095bbeaf0fa304e535c1dddabdd
Priyansh <priyansh.jain@napses.com>
2022-07-15 18:26:39 +0530

    Refactor Master

--- deployment-scripts/connect-to-qa.sh
+++
@@ -1,1 +0,0 @@
-ssh -i ../keys/napsesdevelopment.pem ubuntu@ec2-13-234-116-224.ap-south-1.compute.amazonaws.com
--- deployment-scripts/start-scripts/start_prod_ui.sh
+++
@@ -1,7 +0,0 @@
-cd /home/ubuntu/apps/prod/web
-npm install
-npm run build:production 
-pm2 stop web-prod-hafla
-pm2 restart web-prod-hafla
-# pm2 start npm --name "web-prod-hafla" -- start 
-
