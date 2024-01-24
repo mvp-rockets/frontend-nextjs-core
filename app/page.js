002fe88c7e1c54af994d4863ed99c553de133b3f
Akash Kumar <akash.kumar@napses.com>
2024-01-24 13:26:33 +0530

    use node --lts

--- app/page.js
+++ app/page.js
@@ -1,8 +1,16 @@
-import LinkButton from "atoms/LinkButton"
+import Logo from "@/atoms/Logo"
+import LinkButton from "@/atoms/LinkButton"
 
 export default function Page({ params, searchParams }) {
   return <div className="flex flex-col items-center justify-center h-screen w-screen bg-red-100">
-    <h1 className="text-red-400 font-bold text-[100px]">My Page</h1>
-    <LinkButton  href="/home" >Home Page</LinkButton>
+    <Logo
+      src="/images/logo.svg"
+      className=''
+      width='149'
+      height="40"
+      alt='MVP Rockets'
+    />
+    <h1 className="text-red-400 font-bold text-[100px]">My App</h1>
+    <LinkButton href="/home" >Home Page</LinkButton>
   </div>
 }
