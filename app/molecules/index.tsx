132f2182eb316e0c4c13541bb593007afddbb35a
dhaval <dhaval@napses.com>
2024-07-08 11:34:19 +0530

    removed unwanted code

--- app/molecules/index.tsx
+++ app/molecules/index.tsx
@@ -2,8 +2,6 @@ import AnimatedInputLabel from "./AnimatedInputLabel";
 import AvatarGroup from "./AvatarGroup";
 import Breadcrumbs from "./Breadcrumbs";
 import Card from "./Card";
-import Chart from "./Chart";
-import Container from "./Container";
 import Dropdown from "./Dropdown";
 import MultiSelectDropdown from "./MultiSelectDropdown";
 import Stepper from "./Stepper";
@@ -11,15 +9,12 @@ import Searchbar from "./Searchbar";
 import TimeLine from "./Timelines";
 import Datepicker from "./Datepicker";
 import Pagination from "./Pagination";
-import FileUpload from "./FileUpload";
 
 export {
     AnimatedInputLabel,
     AvatarGroup,
     Breadcrumbs,
     Card,
-    Chart,
-    Container,
     Dropdown,
     MultiSelectDropdown,
     Searchbar,
@@ -27,5 +22,4 @@ export {
     TimeLine,
     Datepicker,
     Pagination,
-    FileUpload
 }
