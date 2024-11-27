2a113c1eb378ae12881bffa1c11305d4f98b69fd
Hiren Thacker <hithacker@gmail.com>
2024-11-27 11:43:11 +0530

    Adding start scripts and enabling nextjs support in storybook

---
+++ start_storybook.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -e
+echo "Loading nvm"
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
+echo "Starting Storybook for env dev"
+cd ui && nvm use && npm install && npm run storybook
